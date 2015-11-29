38eb50f2d5da7e29e64bd1281464b46ab2d8ab83
Jason Bedard <jason+github@jbedard.ca>
2015-11-28 17:04:01 -0800

    refactor($compile): avoid creating temporary closure in assertNoDuplicate

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -2488,14 +2488,13 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
       return a.index - b.index;
     }
 
+    function wrapModuleNameIfDefined(moduleName) {
+      return moduleName ?
+        (' (module: ' + moduleName + ')') :
+        '';
+    }
+
     function assertNoDuplicate(what, previousDirective, directive, element) {
-
-      function wrapModuleNameIfDefined(moduleName) {
-        return moduleName ?
-          (' (module: ' + moduleName + ')') :
-          '';
-      }
-
       if (previousDirective) {
         throw $compileMinErr('multidir', 'Multiple directives [{0}{1}, {2}{3}] asking for {4} on: {5}',
             previousDirective.name, wrapModuleNameIfDefined(previousDirective.$$moduleName),
